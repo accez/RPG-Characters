59ba3ad8692cd89cc07799702fcc5a807745ad9b
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-02-10 13:17:58 +0100

    feat: getters and setters for Character class

--- src/com/company/Character.java
+++ src/com/company/Character.java
@@ -1,15 +1,30 @@
 package com.company;
 
 public abstract class Character {
-    String name;
-    int level;
-    PrimaryAttribute attribute = new PrimaryAttribute();
+    private String name;
+    private int level;
+    public PrimaryAttribute attribute = new PrimaryAttribute();
 
     public Character(String name) {
         this.name = name;
         this.level = 1;
     }
 
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public abstract void levelUp();
 }
