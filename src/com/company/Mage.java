e51d606a07f07362b49bf9356f48fdc8183ca785
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-02-10 09:37:43 +0100

    Feat: Mage class added

---
+++ src/com/company/Mage.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Mage extends Character{
+
+    public Mage(String name){
+        super(name);
+        attribute.strength = 1;
+        attribute.dexterity = 1;
+        attribute.intelligence = 8;
+    }
+
+    @Override
+    public void levelUp() {
+        attribute.strength += 1;
+        attribute.dexterity += 1;
+        attribute.intelligence +=5;
+    }
+}
