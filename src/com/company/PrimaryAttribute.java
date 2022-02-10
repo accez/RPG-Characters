dfc8b883ada390afccb442223f2569835a5ca650
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-02-10 13:15:32 +0100

    feat: getters and setters for PrimaryAttributes

--- src/com/company/PrimaryAttribute.java
+++ src/com/company/PrimaryAttribute.java
@@ -1,8 +1,32 @@
 package com.company;
 
 public class PrimaryAttribute {
-    int strength;
-    int dexterity;
-    int intelligence;
+     private int strength;
+     private int dexterity;
+     private int intelligence;
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
 }
 
