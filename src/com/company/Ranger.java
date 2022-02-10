85377048459052ffec2839ff739b434652871776
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-02-10 09:43:32 +0100

    feat: Ranger class added

---
+++ src/com/company/Ranger.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Ranger extends Character {
+    public Ranger(String name) {
+        super(name);
+        attribute.strength = 1;
+        attribute.dexterity = 7;
+        attribute.intelligence = 1;
+    }
+
+    @Override
+    public void levelUp() {
+        attribute.strength += 1;
+        attribute.dexterity += 5;
+        attribute.intelligence += 1;
+    }
+}
