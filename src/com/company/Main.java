759f3cf2eb64e5e34d75f41d8fe746d8b122d1d1
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-02-11 09:37:10 +0100

    feat: Main running the game

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,18 @@
 package com.company;
+
+import com.company.Game.Game;
+
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        Game game = new Game();
+        while (game.exitGame()) {
+            game.gameState(scanner.nextInt());
+        }
     }
+
 }
