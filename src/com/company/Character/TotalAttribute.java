a2a5fada836716c19316b19f67bed10a1a3f70b0
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-02-14 10:30:46 +0100

    feat: add TotalAttribute class

---
+++ src/com/company/Character/TotalAttribute.java
@@ -0,0 +1,33 @@
+package com.company.Character;
+
+public class TotalAttribute {
+    private int strength;
+    private int dexterity;
+    private int intelligence;
+
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+}
