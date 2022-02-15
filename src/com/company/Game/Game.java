6bac3d5ee0b4266b21a4a7a9a72d766160284917
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-02-15 09:18:45 +0100

    feat: Add error handeling

--- src/com/company/Game/Game.java
+++ src/com/company/Game/Game.java
@@ -1,6 +1,8 @@
 package com.company.Game;
 
 import com.company.Character.CharacterClasses.*;
+import com.company.ErrorHandling.InvalidArmor;
+import com.company.ErrorHandling.InvalidWeapon;
 import com.company.Item.Armor.Armor;
 import com.company.Item.Armor.ArmorTypes;
 import com.company.Character.Character;
@@ -61,9 +63,9 @@ public class Game {
         }
     }
 
-    public void equipArmor(String name, int requiredLevel,Slot slot, ArmorTypes armorTypes,int strength,int dexterity, int intelligence) {
+    public void equipArmor(String name, int requiredLevel, Slot slot, ArmorTypes armorTypes, int strength, int dexterity, int intelligence) {
         Armor armor = new Armor(name, requiredLevel, slot, armorTypes);
-        if(character.getLevel() >= armor.getRequiredLevel()){
+        if (character.getLevel() >= armor.getRequiredLevel()) {
             armor.primaryAttribute.setStrength(strength);
             armor.primaryAttribute.setDexterity(dexterity);
             armor.primaryAttribute.setIntelligence(intelligence);
@@ -71,14 +73,14 @@ public class Game {
             character.getEquipment().forEach((key, value) -> System.out.println(key + " " + value.getName()));
             updateTotalAttributes(armor);
             mainMenu();
-        }else{
+        } else {
             System.out.println("Not enough level! You need  to be level " + armor.getRequiredLevel());
             mainMenu();
         }
 
     }
 
-    public void updateTotalAttributes(Armor armor){
+    public void updateTotalAttributes(Armor armor) {
         int totalStrength = armor.primaryAttribute.getStrength() + character.attribute.getStrength();
         int totalDexterity = armor.primaryAttribute.getDexterity() + character.attribute.getDexterity();
         int totalIntelligence = armor.primaryAttribute.getIntelligence() + character.attribute.getIntelligence();
@@ -87,30 +89,30 @@ public class Game {
         character.totalAttribute.setIntelligence(totalIntelligence);
     }
 
-    public void equipWeapon(String name, int requiredLevel,Slot slot, WeaponTypes weaponTypes,int damage, int attackSpeed) {
-       weapon = new Weapon(name,requiredLevel,slot,weaponTypes,damage,attackSpeed);
-        if(character.getLevel() >= weapon.getRequiredLevel()){
+    public void equipWeapon(String name, int requiredLevel, Slot slot, WeaponTypes weaponTypes, int damage, int attackSpeed) {
+        weapon = new Weapon(name, requiredLevel, slot, weaponTypes, damage, attackSpeed);
+        if (character.getLevel() >= weapon.getRequiredLevel()) {
             character.getEquipment().put(weapon.getSlot(), weapon);
             character.getEquipment().forEach((key, value) -> System.out.println(key + " " + value.getName()));
             mainMenu();
-        }else{
+        } else {
             System.out.println("Not enough level! You need to be level " + weapon.getRequiredLevel());
             mainMenu();
         }
 
     }
 
-    public double characterDPS(MainPrimaryAttribute mainPrimaryAttribute){
-        if(weapon != null){
-                if (mainPrimaryAttribute == MainPrimaryAttribute.STRENGTH){
-                    return weapon.dps() * (1 + character.totalAttribute.getStrength() / 100d);
-                }
-                if (mainPrimaryAttribute == MainPrimaryAttribute.DEXTERITY){
-                    return weapon.dps() * (1 + character.totalAttribute.getDexterity() / 100d);
-                }
-                if (mainPrimaryAttribute == MainPrimaryAttribute.INTELLIGENCE){
-                    return weapon.dps() * (1 + character.totalAttribute.getIntelligence() / 100d);
-                }
+    public double characterDPS(MainPrimaryAttribute mainPrimaryAttribute) {
+        if (weapon != null) {
+            if (mainPrimaryAttribute == MainPrimaryAttribute.STRENGTH) {
+                return weapon.dps() * (1 + character.totalAttribute.getStrength() / 100d);
+            }
+            if (mainPrimaryAttribute == MainPrimaryAttribute.DEXTERITY) {
+                return weapon.dps() * (1 + character.totalAttribute.getDexterity() / 100d);
+            }
+            if (mainPrimaryAttribute == MainPrimaryAttribute.INTELLIGENCE) {
+                return weapon.dps() * (1 + character.totalAttribute.getIntelligence() / 100d);
+            }
         }
         return 0;
     }
@@ -119,116 +121,124 @@ public class Game {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Select slot you want to equip (1. Head, 2. Body, 3. Legs, 4. Weapon)");
         int input = scanner.nextInt();
-        switch (input) {
-            case 1 -> {
-                setSlot(Slot.HEAD);
-                selectArmorType();
-            }
-            case 2 -> {
-                setSlot(Slot.BODY);
-                selectArmorType();
-            }
-            case 3 -> {
-                setSlot(Slot.LEGS);
-                selectArmorType();
-            }
-            case 4 -> {
-                setSlot(Slot.WEAPON);
-                selectWeaponType();
-            }
-        }
-    }
-
-    public void selectWeaponType(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Select your weapon type(1. Axe, 2. Bow, 3. Dagger, 4. Hammer, 5. Staffs, 6. Swords, 7. Wands)");
-        int input = scanner.nextInt();
-        switch (input) {
-            case 1 -> checkIfHeroCanUseWeapon(HeroType.WARRIOR,WeaponTypes.AXES);
-            case 2 -> checkIfHeroCanUseWeapon(HeroType.RANGER,WeaponTypes.BOWS);
-            case 3 -> checkIfHeroCanUseWeapon(HeroType.ROUGE,WeaponTypes.DAGGERS);
-            case 4 -> checkIfHeroCanUseWeapon(HeroType.WARRIOR,WeaponTypes.HAMMERS);
-            case 5 -> checkIfHeroCanUseWeapon(HeroType.MAGE,WeaponTypes.STAFFS);
-            case 6 -> {
-                if(character.getHeroType() == HeroType.WARRIOR){
-                    setWeaponTypes(weaponTypes);
-                    createItem(getSlot(),armorTypes,getWeaponTypes());
-                }else if (character.getHeroType() == HeroType.ROUGE){
-                    setWeaponTypes(weaponTypes);
-                    createItem(getSlot(),armorTypes,getWeaponTypes());
-                }else{
-                    System.out.println("You need to be a warrior or a rouge to equip that");
+        try {
+            switch (input) {
+                case 1 -> {
+                    setSlot(Slot.HEAD);
+                    selectArmorType();
+                }
+                case 2 -> {
+                    setSlot(Slot.BODY);
+                    selectArmorType();
+                }
+                case 3 -> {
+                    setSlot(Slot.LEGS);
+                    selectArmorType();
+                }
+                case 4 -> {
+                    setSlot(Slot.WEAPON);
                     selectWeaponType();
                 }
             }
-            case 7 -> checkIfHeroCanUseWeapon(HeroType.MAGE,WeaponTypes.WANDS);
+        } catch (InvalidArmor e) {
+            e.printStackTrace();
+            selectItemSlot();
         }
+
     }
 
-    public void checkIfHeroCanUseWeapon(HeroType heroType, WeaponTypes weaponTypes){
-        String heroTypeToString = heroType.toString().toLowerCase(Locale.ROOT);
-        String weaponTypeToString = weaponTypes.toString().toLowerCase(Locale.ROOT);
-        if(character.getHeroType() == heroType){
-            setWeaponTypes(weaponTypes);
-            createItem(getSlot(),armorTypes,getWeaponTypes());
-        }else{
-            System.out.println("You need to be a " + heroTypeToString+" in order to equip " + weaponTypeToString );
+    public void selectWeaponType() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select your weapon type(1. Axe, 2. Bow, 3. Dagger, 4. Hammer, 5. Staffs, 6. Swords, 7. Wands)");
+        int input = scanner.nextInt();
+        try{
+            switch (input) {
+                case 1 -> checkIfHeroCanUseWeapon(HeroType.WARRIOR, WeaponTypes.AXES);
+                case 2 -> checkIfHeroCanUseWeapon(HeroType.RANGER, WeaponTypes.BOWS);
+                case 3 -> checkIfHeroCanUseWeapon(HeroType.ROUGE, WeaponTypes.DAGGERS);
+                case 4 -> checkIfHeroCanUseWeapon(HeroType.WARRIOR, WeaponTypes.HAMMERS);
+                case 5 -> checkIfHeroCanUseWeapon(HeroType.MAGE, WeaponTypes.STAFFS);
+                case 6 -> checkIfHeroCanUseSword();
+                case 7 -> checkIfHeroCanUseWeapon(HeroType.MAGE, WeaponTypes.WANDS);
+            }
+        }catch (InvalidWeapon e){
+            e.printStackTrace();
             selectWeaponType();
         }
+
     }
 
-    public void selectArmorType(){
+    public void checkIfHeroCanUseSword() throws InvalidWeapon{
+        if (character.getHeroType() == HeroType.WARRIOR) {
+            setWeaponTypes(weaponTypes);
+            createItem(getSlot(), armorTypes, getWeaponTypes());
+        } else if (character.getHeroType() == HeroType.ROUGE) {
+            setWeaponTypes(weaponTypes);
+            createItem(getSlot(), armorTypes, getWeaponTypes());
+        } else {
+            throw new InvalidWeapon("You need to be a warrior or a rouge to equip that");
+        }
+    }
+
+    public void checkIfHeroCanUseWeapon(HeroType heroType, WeaponTypes weaponTypes) throws InvalidWeapon{
+        String heroTypeToString = heroType.toString().toLowerCase(Locale.ROOT);
+        String weaponTypeToString = weaponTypes.toString().toLowerCase(Locale.ROOT);
+        if (character.getHeroType() == heroType) {
+            setWeaponTypes(weaponTypes);
+            createItem(getSlot(), armorTypes, getWeaponTypes());
+        } else {
+            throw new InvalidWeapon("You need to be a " + heroTypeToString + " in order to equip " + weaponTypeToString);
+        }
+    }
+
+    public void selectArmorType() throws InvalidArmor {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Select your armor type(1. Cloth, 2. Leather, 3. Mail, 4. Plate)");
         int input = scanner.nextInt();
         switch (input) {
             case 1 -> {
-                if(character.getHeroType() == HeroType.MAGE){
+                if (character.getHeroType() == HeroType.MAGE) {
                     setArmorTypes(ArmorTypes.CLOTH);
-                    createItem(getSlot(),getArmorTypes(),weaponTypes);
-                }else{
-                    System.out.println("You need to be a mage to equip that");
-                    selectArmorType();
+                    createItem(getSlot(), getArmorTypes(), weaponTypes);
+                } else {
+                    throw new InvalidArmor("Needs to be a mage to equip that");
                 }
             }
             case 2 -> {
-                if(character.getHeroType() == HeroType.RANGER){
+                if (character.getHeroType() == HeroType.RANGER) {
                     setArmorTypes(ArmorTypes.LEATHER);
-                    createItem(getSlot(),getArmorTypes(),weaponTypes);
-                }else if(character.getHeroType() == HeroType.ROUGE){
+                    createItem(getSlot(), getArmorTypes(), weaponTypes);
+                } else if (character.getHeroType() == HeroType.ROUGE) {
                     setArmorTypes(ArmorTypes.LEATHER);
-                    createItem(getSlot(),getArmorTypes(),weaponTypes);
-                }else{
-                    System.out.println("You need to be a rouge or a ranger to equip that");
-                    selectArmorType();
+                    createItem(getSlot(), getArmorTypes(), weaponTypes);
+                } else {
+                    throw new InvalidArmor("You need to be a rouge or a ranger to equip that");
                 }
             }
             case 3 -> {
-                if(character.getHeroType() != HeroType.MAGE){
+                if (character.getHeroType() != HeroType.MAGE) {
                     setArmorTypes(ArmorTypes.MAIL);
-                    createItem(getSlot(),getArmorTypes(),weaponTypes);
-                }else{
-                    System.out.println("You need to be a rouge, ranger or a warrior to equip that");
-                    selectArmorType();
+                    createItem(getSlot(), getArmorTypes(), weaponTypes);
+                } else {
+                    throw new InvalidArmor("You need to be a rouge, ranger or a warrior to equip that");
                 }
             }
             case 4 -> {
-                if (character.getHeroType() == HeroType.WARRIOR){
+                if (character.getHeroType() == HeroType.WARRIOR) {
                     setArmorTypes(ArmorTypes.PLATE);
-                    createItem(getSlot(),getArmorTypes(),weaponTypes);
-                }else{
-                    System.out.println("You need to be a warrior to equip that");
-                    selectArmorType();
+                    createItem(getSlot(), getArmorTypes(), weaponTypes);
+                } else {
+                    throw new InvalidArmor("You need to be a warrior to equip that");
                 }
             }
         }
     }
 
-    public void createItem(Slot slot, ArmorTypes armorType, WeaponTypes weaponTypes){
+    public void createItem(Slot slot, ArmorTypes armorType, WeaponTypes weaponTypes) {
         String[] items = {"Basic", "Great", "Master"};
         ArrayList<String> createdItems = new ArrayList<>();
 
-        if(slot == Slot.WEAPON){
+        if (slot == Slot.WEAPON) {
             System.out.println("Select your weapon:");
             String weaponTypeName = weaponTypes.toString().toLowerCase(Locale.ROOT);
             for (int i = 0; i < items.length; i++) {
@@ -238,11 +248,11 @@ public class Game {
             Scanner scanner = new Scanner(System.in);
             int input = scanner.nextInt();
             switch (input) {
-                case 1 -> equipWeapon(createdItems.get(0), 1, slot, weaponTypes,2,2);
-                case 2 -> equipWeapon(createdItems.get(1), 5, slot, weaponTypes,5,5);
-                case 3 -> equipWeapon(createdItems.get(2), 10, slot, weaponTypes,10,10);
+                case 1 -> equipWeapon(createdItems.get(0), 1, slot, weaponTypes, 2, 2);
+                case 2 -> equipWeapon(createdItems.get(1), 5, slot, weaponTypes, 5, 5);
+                case 3 -> equipWeapon(createdItems.get(2), 10, slot, weaponTypes, 10, 10);
             }
-        }else{
+        } else {
             System.out.println("Select your armor:");
             String armorTypeName = armorType.toString().toLowerCase(Locale.ROOT);
             for (int i = 0; i < items.length; i++) {
@@ -252,9 +262,9 @@ public class Game {
             Scanner scanner = new Scanner(System.in);
             int input = scanner.nextInt();
             switch (input) {
-                case 1 -> equipArmor(createdItems.get(0), 1, slot, armorTypes,2,2,2);
-                case 2 -> equipArmor(createdItems.get(1), 5, slot, armorTypes,5,5,5);
-                case 3 -> equipArmor(createdItems.get(2), 10, slot, armorTypes,10,10,10);
+                case 1 -> equipArmor(createdItems.get(0), 1, slot, armorTypes, 2, 2, 2);
+                case 2 -> equipArmor(createdItems.get(1), 5, slot, armorTypes, 5, 5, 5);
+                case 3 -> equipArmor(createdItems.get(2), 10, slot, armorTypes, 10, 10, 10);
             }
         }
     }
@@ -294,7 +304,7 @@ public class Game {
                 return true;
             }
             case 4 -> {
-                Warrior warrior = new Warrior(name, HeroType.WARRIOR,MainPrimaryAttribute.STRENGTH);
+                Warrior warrior = new Warrior(name, HeroType.WARRIOR, MainPrimaryAttribute.STRENGTH);
                 System.out.println("You created a Warrior with the name " + warrior.getName());
                 setCharacter(warrior);
                 return true;
@@ -304,12 +314,12 @@ public class Game {
     }
 
     public void displayUserStats() {
-        if(character.getEquipment().isEmpty()){
+        if (character.getEquipment().isEmpty()) {
             System.out.println("Strength: " + getCharacter().attribute.getStrength());
             System.out.println("Dexterity: " + getCharacter().attribute.getDexterity());
             System.out.println("Intelligence: " + getCharacter().attribute.getIntelligence());
             mainMenu();
-        }else{
+        } else {
             System.out.println("Strength: " + getCharacter().totalAttribute.getStrength());
             System.out.println("Dexterity: " + getCharacter().totalAttribute.getDexterity());
             System.out.println("Intelligence: " + getCharacter().totalAttribute.getIntelligence());
@@ -362,11 +372,11 @@ public class Game {
         this.armorTypes = armorTypes;
     }
 
-    public WeaponTypes getWeaponTypes(){
+    public WeaponTypes getWeaponTypes() {
         return weaponTypes;
     }
 
-    public void setWeaponTypes(WeaponTypes weaponTypes){
+    public void setWeaponTypes(WeaponTypes weaponTypes) {
         this.weaponTypes = weaponTypes;
     }
 }
